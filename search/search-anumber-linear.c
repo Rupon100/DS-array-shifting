8c842c636b5af8c7d555a2995f8724a978ef597e
Rupon100 <ruponmia97@gmail.com>
2025-07-19 19:09:58 +0600

    binary search with external function

--- search/search-anumber-linear.c
+++ search/search-anumber-linear.c
@@ -57,3 +57,58 @@ int main(){
     return 0;
 }
 
+
+
+// #include <stdio.h>
+
+// // Function to count occurrences of key using binary search
+// int countOccurrences(int arr[], int size, int key) {
+//     int initial = 0;
+//     int end = size - 1;
+//     int count = 0;
+//     int mid;
+
+//     while (initial <= end) {
+//         mid = (initial + end) / 2;
+
+//         if (arr[mid] == key) {
+//             count = 1;
+
+//             // Check to the right
+//             int i = mid + 1;
+//             while (i < size && arr[i] == key) {
+//                 count++;
+//                 i++;
+//             }
+
+//             // Check to the left
+//             i = mid - 1;
+//             while (i >= 0 && arr[i] == key) {
+//                 count++;
+//                 i--;
+//             }
+
+//             break;
+//         } else if (arr[mid] < key) {
+//             initial = mid + 1;
+//         } else {
+//             end = mid - 1;
+//         }
+//     }
+
+//     return count;
+// }
+
+// int main() {
+//     int arr[20] = {1, 2, 3, 4, 5, 6, 7, 7, 7, 10, 11, 11, 11, 11, 15, 16, 17, 18, 19, 20};
+//     int key = 7;
+
+//     int count = countOccurrences(arr, 20, key);
+
+//     if (count > 0)
+//         printf("%d is %d times here\n", key, count);
+//     else
+//         printf("%d is not in the array\n", key);
+
+//     return 0;
+// }
